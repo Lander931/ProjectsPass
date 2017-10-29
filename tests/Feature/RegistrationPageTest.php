f9049ec254e2806261ca9cbc77104db41d01f313
Lander931 <Lander931@mail.ru>
2017-10-29 20:31:06 +0300

    Add tests

--- tests/Feature/RegistrationPageTest.php
+++ tests/Feature/RegistrationPageTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Faker\Factory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -14,5 +16,37 @@ class RegistrationPageTest extends TestCase
         $response = $this->get(route('register',[],false));
         
         $response->assertStatus(200);
+    }
+
+    public function testRegistrationFailedAction()
+    {
+        $response = $this->post(route('register',[],false));
+        
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+    }
+
+    public function testRegistrationAction()
+    {
+        $name = Factory::create()->name;
+        $email = Factory::create()->email;
+        $password = Factory::create()->password();
+
+        $response = $this->post(route('register'),[
+            'name'                  => $name,
+            'email'                 => $email,
+            'password'              => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('home'));
+
+        $user = User::whereEmail($email)->first();
+
+        $this->assertNotNull($user);
+        $this->assertEquals($user->name,$name);
+        $this->assertNotEquals($user->password,$password);
+        $this->assertTrue(password_verify($password,$user->password));
     }
 }
