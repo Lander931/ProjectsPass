07a1f2b7ff4572091bbacaedb91cc18b0215acf7
Lander931 <Lander931@mail.ru>
2017-10-30 08:36:01 +0300

    Add Test

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginTest extends TestCase
+{
+    public function testLoginPageAvialable()
+    {
+        $response = $this->get(route('login'));
+        
+        $response->assertStatus(200);
+    }
+    public function testLoginFailedAction()
+    {
+        $response = $this->post(route('login'));
+        
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
+    }
+}
