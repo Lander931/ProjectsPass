925eb14c956f0db4dcd442b362404e06b14f35aa
Lander931 <Lander931@mail.ru>
2017-11-04 14:06:07 +0300

    Add test

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -10,5 +10,22 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ProjectTest extends TestCase
 {
-    
+    public function testShowProjectList()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->post(route('login'));
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/project');
+    }
+
+    public function testCreateProjectPageAvailable(){
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->post(route('login'));
+        
+        $response = $this->get(route('project.create'));
+        $response->assertStatus(200);
+    }
 }
