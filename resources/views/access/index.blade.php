e79b5e1b792cde4965f887d8a994802b507680cc
Lander931 <Lander931@mail.ru>
2017-11-17 21:20:45 +0300

    Изменено отображение Access на странице проекта

--- resources/views/access/index.blade.php
+++ resources/views/access/index.blade.php
@@ -1,16 +1,3 @@
-@section('script')
-    <script>
-        $('.crypt').click(function () {
-            $('.crypt').css('display','none');
-            $('.decrypt').css('display','inline-block');
-        });
-        $('.decrypt').click(function () {
-            $('.decrypt').css('display','none');
-            $('.crypt').css('display','inline-block');
-        });
-    </script>
-@endsection
-
 <div class="panel panel-default">
     <div class="panel-heading">
         Доступы
@@ -24,12 +11,24 @@
         <tbody>
         @foreach($accesses as $access)
             <tr>
-                <td>{{$access->type}}</td>
-                <td>{{$access->login}}</td>
-                <td><span class="crypt">********</span><span class="decrypt"
-                                                             style="display: none">{{decrypt($access->password)}}</span>
+                <td>
+                    <div><b>{{$access->type}}</b></div>
+                    <div style="font-size:70%; color: gray">{{$access->comment}}</div>
                 </td>
-                <td>{{$access->comment}}</td>
+                <td>
+                    <div>
+                        <i class="fa fa-user icon-access" aria-hidden="true"></i><span>{{$access->login}}</span></div>
+                    <div>
+                        <i class="fa fa-unlock-alt icon-access" aria-hidden="true"></i><span>{{decrypt($access->password)}}</span>
+                    </div>
+                </td>
+                <td width="40px">
+                    <a style="margin-bottom: 2px" href="" class="btn btn-default btn-xs">
+                        <span class="glyphicon glyphicon-edit"></span>
+                    </a>
+                    <button type="button" class="btn btn-default btn-xs" data-toggle="modal"
+                            data-target="#modalDeleteNote"><span class="glyphicon glyphicon-trash"></span></button>
+                </td>
             </tr>
         @endforeach
         </tbody>
