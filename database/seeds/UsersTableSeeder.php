0efd936d3ed24d607eb71d742a4f2b8d2875d8f4
Lander931 <Lander931@mail.ru>
2017-11-18 14:04:53 +0300

    + стили/шрифты/seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create(['name' => 'admin','email' => 'admin@admin.ru','password' => bcrypt('admin')]);
+    }
+}
